2f02e8c244a00455db80343c96a56b43a030bef1
AlexAasen <alexaasen@outlook.com>
2019-09-19 13:27:55 +0200

    testing a publish

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-app.use('/bundles', express.static('bundles'))
+app.use('/build', express.static('build'))
 app.use('/src', express.static('src'))
 
 app.get('*', function(req, res) {
